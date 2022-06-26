9ab2418fc3f45b4b04f86ebd7a222a5abd87cb26
garman000 <garman000@gmail.com>
2022-06-26 13:45:38 +0300

    added function

--- index.js
+++ index.js
@@ -7,6 +7,20 @@ module.exports = {
 
   travel2: function(){
     console.log("travel2")
-  }
-  };
+  },
+  createData: function formatDate(date1) {
+    let date = new Date(date1),
+      month = "" + (date.getMonth() + 1),
+      day = "" + date.getDate(),
+      year = "" + date.getFullYear();
 
+    if (month.length < 2) month = "0" + month;
+    if (day.length < 2) day = "0" + day;
+
+    let formatedYear = year.slice(2, 4);
+
+    return [formatedYear, month, day].join("");
+  }}
+
+
+  
