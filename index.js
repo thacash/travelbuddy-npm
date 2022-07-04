1a418b4f30862dce3a92ae83e03181838cea3506
garman000 <garman000@gmail.com>
2022-07-04 11:38:03 +0300

    removed import

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
-import airportList from "./airportList"
-
 module.exports = {
+ 
   calculateTilEvent: function days(eventStartDate, todaysDate) {
     let difference = eventStartDate - todaysDate;
     let TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
     return TotalDays;
-
+    
   },
-
-    createData: function formatDate(date1) {
+  
+  createData: function formatDate(date1) {
     let date = new Date(date1),
-      month = "" + (date.getMonth() + 1),
-      day = "" + date.getDate(),
-      year = "" + date.getFullYear();
-
+    month = "" + (date.getMonth() + 1),
+    day = "" + date.getDate(),
+    year = "" + date.getFullYear();
+    
     if (month.length < 2) month = "0" + month;
     if (day.length < 2) day = "0" + day;
-
+    
     let formatedYear = year.slice(2, 4);
-
+    
     return [formatedYear, month, day].join("");
   },
-
-  airportList: airportList,
+  
 };
  
   
